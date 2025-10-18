88b55903a627007e96dcd7d69731941d270b71be
crash <ltung783@gmail.com>
2025-10-18 11:40:51 -0700

    initial commit (fairness constraints currently hacked together)

---
+++ round_robin_cp.py
@@ -0,0 +1,254 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Generate round robin brackets using Constraint Programming (CP), where higher seeded
+teams are guaranteed to have an easier schedule.
+
+To Do:
+
+- Add stats for measuring linearity and fairness of the solution
+- Fix the kludgy 0-based vs. 1-based team/round stuff in `validate_bracket`
+- Take into account difficulty of opponents in adjacent (or near-adjacent) rounds
+"""
+
+from statistics import mean, median
+import sys
+import os
+
+from ortools.sat.python import cp_model
+
+DEBUG = int(os.environ.get('ROUND_ROBIN_DEBUG') or 0)
+
+def build_bracket(nteams: int, nrounds: int) -> list | None:
+    """Attempt to build a bracket with the specified configuration.  Return ``None`` if
+    solver is unable to come up with a solution given the specified parameters and/or
+    bounds.
+
+    If the number of teams is odd, then a "ghost" team is created to pair with the bye
+    team for each round.
+
+    NOTE: this function currently only works for nrounds < nteams < 2 * nrounds
+
+    Constraints:
+
+    1. Each table seats exactly 2 teams (including the ghost, if exists) in every round
+
+    2. Each team (including the ghost) sits at exactly one table in every round
+
+    3. Pairs of teams may not sit at the same table in more than one round (including the
+       ghost)
+
+    4. Additional constraints programmatically generated to generally pair higher seeds
+       with lower seeds (ATTN: algorithm still in development!)
+    """
+    assert nteams > nrounds
+    assert nteams <= 2 * nrounds
+
+    nopps   = nteams - 1
+    ntables = nopps // 2 + 1
+    nghosts = ntables * 2 - nteams
+    tteams  = nteams + nghosts
+    assert nghosts <= 1
+    assert tteams & 0x01 == 0
+
+    teams     = range(nteams)
+    rounds    = range(nrounds)
+    tables    = range(ntables)
+    all_teams = range(tteams)  # includes ghosts
+    ghosts    = all_teams[nteams:]
+
+    model = cp_model.CpModel()
+
+    # Constraint #0 - specify domain for (team, round, table)
+    seats = {}
+    for p in all_teams:
+        for r in rounds:
+            for t in tables:
+                seats[(p, r, t)] = model.new_bool_var(f'seat_p{p}_r{r}_t{t}')
+
+    # Constraint #1 - for every round, each table seats 2 teams
+    for r in rounds:
+        for t in tables:
+            model.add(sum(seats[(p, r, t)] for p in all_teams) == 2)
+
+    # Constraint #2 - every team sits at one table per round
+    for p in all_teams:
+        for r in rounds:
+            model.add(sum(seats[(p, r, t)] for t in tables) == 1)
+
+    # Constraint #3 - every pair of teams meets no more than once across all rounds
+    for p1 in all_teams[:-1]:
+        for p2 in all_teams[p1+1:]:
+            mtgs = []
+            for r in rounds:
+                for t in tables:
+                    mtg = model.new_bool_var(f'mtg_p{p1}_p{p2}_r{r}_t{t}')
+                    model.add_multiplication_equality(mtg, [seats[(p1, r, t)], seats[(p2, r, t)]])
+                    mtgs.append(mtg)
+            model.add(sum(mtgs) < 2)
+
+    # Constraint #4 - additional constraints to pair higher seeds with lower seeds (work
+    # in development--currently very hacky!!!)
+    sched10 = [( 2, 10),
+               ( 2, 10),
+               ( 4, 10),
+               ( 4, 10),
+               ( 6, 10),
+               ( 6, 10),
+               ( 8, 10),
+               ( 8, 10)]
+    sched12 = [( 4, 12),
+               ( 4, 12),
+               ( 4, 12),
+               ( 4, 12),
+               ( 8, 12),
+               ( 8, 12),
+               (10, 12),
+               (10, 12)]
+    sched14 = [( 6, 14),
+               ( 6, 14),
+               ( 6, 14),
+               ( 6, 14),
+               ( 6, 14),
+               ( 6, 14),
+               (12, 14),
+               (12, 14)]
+    sched16 = [( 8, 16),
+               ( 8, 16),
+               ( 8, 16),
+               ( 8, 16),
+               (10, 16),
+               (10, 16),
+               (10, 16),
+               (10, 16)]
+    scheds = {10: sched10,
+              12: sched12,
+              14: sched14,
+              16: sched16}
+    sched = scheds[tteams]
+
+    for p1, (lo, hi) in enumerate(sched):
+        for p2 in range(lo, hi):
+            #print(f"{p1} - {p2}")
+            mtgs = []
+            for r in rounds:
+                for t in tables:
+                    mtg = model.new_bool_var(f'mtg_p{p1}_p{p2}_r{r}_t{t}')
+                    model.add_multiplication_equality(mtg, [seats[(p1, r, t)], seats[(p2, r, t)]])
+                    mtgs.append(mtg)
+            model.add(sum(mtgs) == 1)
+
+    solver = cp_model.CpSolver()
+    if DEBUG:
+        solver.parameters.log_search_progress = True
+        if DEBUG > 1:
+            solver.parameters.log_subsolver_statistics = True
+    status = solver.solve(model)
+    print(f"Status: {status} ({solver.status_name(status)})", file=sys.stderr)
+
+    if status not in (cp_model.OPTIMAL, cp_model.FEASIBLE):
+        return None
+
+    bracket = []
+    for r in rounds:
+        bracket.append([])
+        for t in tables:
+            bracket[-1].append([p for p in all_teams if solver.value(seats[(p, r, t)])])
+
+    if not validate_bracket(bracket, tteams, nrounds):
+        raise RuntimeError("Generated bracket fails validation")
+
+    print("\nSolver Stats", file=sys.stderr)
+    print(f"- Conflicts : {solver.num_conflicts}", file=sys.stderr)
+    print(f"- Branches  : {solver.num_branches}", file=sys.stderr)
+    print(f"- Wall time : {solver.wall_time:.2f} secs", file=sys.stderr)
+    return bracket
+
+def validate_bracket(bracket_in: list, nteams: int, nrounds: int) -> bool:
+    """Return ``True`` if bracket is correct (i.e. all intended constraints met),
+    ``False`` otherwise.
+    """
+    assert nrounds == len(bracket_in)
+
+    # NOTE: index is zero-based, but inner list values are 1-based
+    team_opps = [[] for _ in range(nteams)]
+    all_teams = set(p + 1 for p in range(nteams))
+
+    for round in bracket_in:
+        for table in round:
+            assert len(table) == 2
+            p1, p2 = table
+            team_opps[p1].append(p2+1)
+            team_opps[p2].append(p1+1)
+    assert len(team_opps) == nteams
+
+    stats = []
+    print("\nOpponents by Round")
+    
+    for p, opps in enumerate(team_opps):
+        uniq = set(opps)
+        assert len(uniq) == len(opps)
+        seed = p + 1
+        no_play = all_teams - uniq - set([seed])
+        print(f"{seed:2d}: play: {opps}, no play: {sorted(no_play)}")
+        stats.append((min(opps), max(opps), median(opps), mean(opps)))
+
+    print("\n        Opponent Stats"
+          "\n    Min  Max  Median  Mean"
+          "\n    ---  ---  ------  ----")
+    for idx, st in enumerate(stats):
+        print(f"{idx+1:2d}: {st[0]:3d}  {st[1]:3d}  {st[2]:6.2f}  {st[3]:4.2f}")
+        
+    # LATER: validate strict ordering of schedule difficulty!!!
+    return True
+
+def print_bracket(bracket: list) -> None:
+    """Print human-readable representation of the generated backed (internal format).
+    """
+    for r, round in enumerate(bracket):
+        print(f"\nRound {r+1}:")
+        for t, table in enumerate(round):
+            print(f"  Table {t+1}: {[p+1 for p in table]}")
+
+def print_bracket_csv(bracket: list) -> None:
+    """Print CSV for generated bracket, compatible with input format expected by
+    ``tourn_eval`` (which doesn't really exist yet!).
+    """
+    for round in bracket:
+        print(','.join([str(team + 1) for table in round for team in table]))
+
+########
+# main #
+########
+
+def main() -> int:
+    """Usage::
+
+      $ python -m round_robin_cp <nteams> <nrounds> [<csvout>]
+
+    where any non-empty value for ``csvout`` specifies that the bracket should be output
+    in CSV format (as expected by ``tourn_eval``); otherwise a human-readable version is
+    printed.
+    """
+    nteams  = int(sys.argv[1])
+    nrounds = int(sys.argv[2])
+    csvout  = None
+    if len(sys.argv) > 3:
+        csvout = bool(sys.argv[3])
+        if len(sys.argv) > 4:
+            print(f"Invalid arg(s): {' '.join(sys.argv[4:])}", file=sys.stderr)
+            return 1
+
+    bracket = build_bracket(nteams, nrounds)
+    if not bracket:
+        print("Unable to build bracket", file=sys.stderr)
+        return 1
+
+    if csvout:
+        print_bracket_csv(bracket)
+    else:
+        print_bracket(bracket)
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
